d5c9c84a371eaf15f8c15553469dff91ae927cda
Michael Hatch <michaelhatch@me.com>
2020-12-18 19:23:52 -0800

    Update symbols.js

--- src/solver/symbols.js
+++ src/solver/symbols.js
@@ -1,3 +1,17 @@
-export const BRANCHES_KEY = Symbol("Branches");
-export const FINISHES_WORD = Symbol("Finishes Word");
-export const PARENT_BRANCH = Symbol("Parent Branch");
+const labels = [
+  "BRANCHES",
+  "COLUMN_INDEX",
+  "FINISHES_WORD",
+  "IS_DOWN",
+  "NUMBER_OF_COLUMNS",
+  "NUMBER_OF_ROWS",
+  "PARENT_BRANCH",
+  "ROW_INDEX",
+  "ROWS",
+  "WORD_ARRAY",
+];
+const symbols = {};
+labels.forEach((label) => {
+  symbols[label] = Symbol(label);
+});
+export default symbols;
