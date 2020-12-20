f3f511ddc9d413847fbb3231595b18b489e7f522
Michael Hatch <michaelhatch@me.com>
2020-12-20 04:22:50 -0800

    Update solver.js

--- src/models/solver.js
+++ src/models/solver.js
@@ -1,54 +1,47 @@
 import { BLACKLIST, BLACKLIST_STRING, CURRENT_SOLVE, READY, TRAY, TRAY_STRING, UPDATE_DATA, UPDATE_FUNCTION } from "./util/symbols";
 
-import { downloadAndUnPackTrie } from "../util/trie";
+import { downloadAndUnPackTrie } from "./trie";
 
-import createSolve from "./solve";
-import createTray from "./tray";
+import createSolve from "../models/solve";
+import createTray from "../models/tray";
 
-class Solver {
-  constructor() {
-    this.data = new Map();
-    downloadAndUnPackTrie().then(() => {
-      const updateConfig = new Map();
-      updateConfig.set(READY, true);
-      this.data.get(UPDATE_FUNCTION)(updateConfig);
-    });
+const data = new Map();
+
+downloadAndUnPackTrie().then(() => {
+  const updateConfig = new Map();
+  updateConfig.set(READY, true);
+  data.get(UPDATE_FUNCTION)(updateConfig);
+});
+
+export const onUpdate = (update) => {
+  data.set(UPDATE_FUNCTION, update);
+};
+
+export const solve = (trayStr, blacklistStr) => {
+  if (blacklistStr) {
+    data.set(BLACKLIST_STRING, blacklistStr);
+  } else if (!data.has(BLACKLIST_STRING)) {
+    data.set(BLACKLIST_STRING, "");
   }
 
-  onUpdate(update) {
-    this.data.set(UPDATE_FUNCTION, update);
+  if (trayStr) {
+    data.set(TRAY_STRING, blacklistStr);
+  } else if (!data.has(TRAY_STRING)) {
+    data.set(TRAY_STRING, "");
   }
 
-  solve(trayStr, blacklistStr) {
-    const $data = this.data;
-    
-    if (blacklistStr) {
-      $data.set(BLACKLIST_STRING, blacklistStr);
-    } else if (!$data.has(BLACKLIST_STRING)) {
-      $data.set(BLACKLIST_STRING, "");
+  const solveConfig = new Map();
+  
+  solveConfig.set(BLACKLIST, new Set(data.get(BLACKLIST_STRING).split(/\s*,\s*/)));
+  solveConfig.set(TRAY, createTray(data.get(TRAY_STRING)));
+
+  solveConfig.set(UPDATE_FUNCTION, (message) => {
+    if (data.get(CURRENT_SOLVE) === message.get(CURRENT_SOLVE)) {
+      data.get(UPDATE_FUNCTION)(message.get(UPDATE_DATA));
+      return true;
     }
-    
-    if (trayStr) {
-      $data.set(TRAY_STRING, blacklistStr);
-    } else if (!$data.has(TRAY_STRING)) {
-      $data.set(TRAY_STRING, "");
-    }
-    
-    const solveConfig = new Map();
-    
-    solveConfig.set(BLACKLIST, new Set($data.get(BLACKLIST_STRING).split(/\s*,\s*/)));
-    solveConfig.set(TRAY, createTray($data.get(TRAY_STRING)));
-    
-    solveConfig.set(UPDATE_FUNCTION, (data) => {
-      if ($data.get(CURRENT_SOLVE) === data.get(CURRENT_SOLVE)) {
-        $data.get(UPDATE_FUNCTION)(data.get(UPDATE_DATA));
-        return true;
-      }
-      return false;
-    });
-    
-    $data.set(CURRENT_SOLVE, createSolve(solveConfig));
-  }
-}
+    return false;
+  });
 
-export default new Solver();
+  data.set(CURRENT_SOLVE, createSolve(solveConfig));
+};
