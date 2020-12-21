161d983996db61c1054605b6f1e3a9e1efd3d16a
Michael Hatch <michaelhatch@me.com>
2020-12-21 11:15:13 -0800

    update

--- src/services/trie.js
+++ src/services/trie.js
@@ -8,10 +8,7 @@ const syms = new Map();
 const wordlistSet = new Set();
 const wordSymbols = new Map();
 
-let i;
-let indexPlusOne;
-let matches;
-let matchesLength;
+let byLetter;
 let newSofar;
 let nextIndex;
 let node;
@@ -22,55 +19,58 @@ let ref;
 
 const processNode = (index, sofar) => {
   node = nodes[index];
+  let matches;
   if (node[0] === "!") {
     processWord(sofar);
     matches = node.slice(1).split(/([A-Z0-9,]+)/g);
   } else {
     matches = node.split(/([A-Z0-9,]+)/g);
   }
-  matchesLength = matches.length;
-  i = 0;
-  while (i < matchesLength) {
+  const matchesLength = matches.length;
+  for (let i = 0; i < matchesLength; i += 2) {
     part = matches[i];
     if (!part) {
-      i += 2;
       continue;
     }
     newSofar = sofar + part;
     ref = matches[i + 1];
     if (ref === "," || ref === undefined) {
       processWord(newSofar);
-      i += 2;
       continue;
     }
     nextIndex = syms.has(ref) ? syms.get(ref) : index + decode(ref) + 1 - numSyms;
     processNode(nextIndex, newSofar);
-    i += 2;
   }
 };
 
 const processWord = (wordStr) => {
   const wordArr = wordStr.split("");
-  const wordSymbol = Symbol();
-  wordArr.reduce((counts, letter) => {
-    counts.set(letter, counts.has(letter) ? counts.get(letter) + 1 : 1);
-    return counts;
-  }, new Map()).forEach((instances, letter) => {
-    if (!byLetterCount.has(letter)) {
-      byLetterCount.set(letter, new Map());
-    }
-    if (!byLetterCount.get(letter).has(instances)) {
-      byLetterCount.get(letter).set(instances, new Set());
-    }
-    byLetterCount.get(letter).get(instances).add(wordSymbol);
-  });
+  const wordSymbol = Symbol(wordStr);
+  wordArr
+    .reduce((counts, letter) => {
+      counts.set(letter, counts.has(letter) ? counts.get(letter) + 1 : 1);
+      return counts;
+    }, new Map())
+    .forEach((instances, letter) => {
+      if (!byLetterCount.has(letter)) {
+        byLetterCount.set(letter, new Map());
+      }
+      byLetter = byLetterCount.get(letter);
+      if (!byLetter.has(instances)) {
+        byLetter.set(instances, new Set());
+      }
+      byLetter.get(instances).add(wordSymbol);
+    });
   wordlistSet.add(wordStr);
   wordSymbols.set(wordSymbol, { wordStr, wordArr });
 };
 
 export const downloadAndUnpackTrie = () => {
   return new Promise((resolve) => {
+    console.time("Compressed words download");
     fetch(wordsTxt.slice(1)).then(async (response) => {
+      console.timeEnd("Compressed words download");
+      console.time("Syms generation");
       const pattern = new RegExp("([0-9A-Z]+):([0-9A-Z]+)");
       nodes = (await response.text()).split(";");
       nodes.some((node) => {
@@ -83,9 +83,26 @@ export const downloadAndUnpackTrie = () => {
       });
       numSyms = syms.size;
       nodes = nodes.slice(numSyms);
+      console.timeEnd("Syms generation");
+      console.time("Generating words from trie");
       processNode(0, "");
-      // TODO: byLetterCount should represent all words that can be made with up to that many of that letter
-      // currently we'd have to get each instances set and add them together in getWordsForSegment()
+      console.timeEnd("Generating words from trie");
+      console.time("Caching word sets");
+      byLetterCount.forEach((instanceMap) => {
+        const sumSet = new Set();
+        [...instanceMap.keys()].sort().forEach((key) => {
+          const wordSet = instanceMap.get(key);
+          wordSet.forEach((wordSymbol) => {
+            sumSet.add(wordSymbol);
+          });
+          sumSet.forEach((wordSymbol) => {
+            if (!wordSet.has(wordSymbol)) {
+              wordSet.add(wordSymbol);
+            }
+          });
+        });
+      });
+      console.timeEnd("Caching word sets");
       resolve();
     });
   });
@@ -93,10 +110,14 @@ export const downloadAndUnpackTrie = () => {
 
 export const getWordsForSegment = (blacklist, segment, tray) => {
   const counts = tray.getCountsWith(segment.getCounts());
-  const alphaKey = [...counts].reduce((str, [letter, count]) => {
-    str += letter.repeat(count);
-    return str;
-  }, "").split("").sort().join("");
+  const alphaKey = [...counts]
+    .reduce((str, [letter, count]) => {
+      str += letter.repeat(count);
+      return str;
+    }, "")
+    .split("")
+    .sort()
+    .join("");
   if (!comboCache.has(alphaKey)) {
     // TODO: create comboCache entry (alphaKey, Set({ wordArr, wordStr }, {..}, ..))
     const entry = new Set();
--- src/util/symbols.js
+++ src/util/symbols.js
@@ -1,10 +1,12 @@
 export const BLACKLIST_STRING = Symbol("BLACKLIST_STRING");
 export const BOARD_ARRAY = Symbol("BOARD_ARRAY");
 export const BY_LETTER_COUNT = Symbol("BY_LETTER_COUNT");
+export const CURRENT_SOLVE = Symbol("CURRENT_SOLVE");
 export const LETTER_COUNTS = Symbol("LETTER_COUNTS");
 export const MESSAGE = Symbol("MESSAGE");
 export const READY = Symbol("READY");
 export const TRAY = Symbol("TRAY");
+export const UPDATE_FUNCTION = Symbol("UPDATE_FUNCTION");
 export const WORD_ARRAY = Symbol("WORD_ARRAY");
 export const WORD_STRING = Symbol("WORD_STRING");
 export const WORD_SYMBOLS = Symbol("WORD_SYMBOLS");
